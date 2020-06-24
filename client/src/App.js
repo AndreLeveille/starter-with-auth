63522a4aefe5c386de1515cf457a252ff47d1fe8
Andre Leveille <andre.joe.leveille@gmail.com>
2020-06-24 01:33:47 -0600

    final code

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar'
+import { Container } from 'semantic-ui-react';
+import Home from './components/Home'
+import Register from './components/Register'
+import Login from './components/Login'
+import FetchUser from './components/FetchUser';
+import ProtectedRoute from './components/ProtectedRoute';
+
+
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <FetchUser>
+        <Container>
+          <Switch>
+            <ProtectedRoute exact path="/" component={Home} />
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </Container>
+      </FetchUser>
+    </>
+ 
+  );
+}
+
+export default App;
---
+++ client/src/providers/AuthProvider.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import axios from 'axios'
+
+export const AuthContext = React.createContext()
+export const AuthConsumer = AuthContext.Consumer
+
+export default class AuthProvider extends React.Component {
+  state = {user: null}
+
+  handleRegister = (user, history) => {
+    console.log("handleRegister called");
+    axios
+      .post("/api/auth", user)
+      .then((res) => {
+        this.setState({ user: res.data.data });
+        history.push("/");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  handleLogin = (user, history) => {
+    axios
+      .post("/api/auth/sign_in", user)
+      .then((res) => {
+        this.setState({ user: res.data.data });
+        history.push("/");
+      })
+      .catch((e) => {
+        alert("Invalid login attempt");
+      });
+  };
+
+  handleLogout = (history) => {
+    axios.delete("/api/auth/sign_out").then((res) => {
+      console.log(res);
+      this.setState({ user: null });
+      history.push("/login");
+    });
+  };
+
+  render (){
+    return(
+      <AuthContext.Provider 
+        value={{
+          ...this.state,
+          authenticated: this.state.user !== null,
+          handleLogin: this.handleLogin,
+          handleRegister: this.handleRegister,
+          handleLogout: this.handleLogout,
+          setUser: (user) => this.setState({user,}),
+        }}
+      >
+        {this.props.children}
+
+      </AuthContext.Provider>
+    )
+  }
+}
